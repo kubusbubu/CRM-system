5c181c20809e48186e0b363eddcc2c207c7c123a
kubusbubu <kubakarczewski25@gmail.com>
2023-11-08 23:24:34 +0100

    models innit

--- leads/models.py
+++ leads/models.py
@@ -1,19 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
 
 
 class Lead(models.Model):
-
-    SOURCE_CHOICES = (
-        ('Youtube', 'Youtube'),
-        ('Google', 'Google'),
-    )
-
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+    #tutaj bedzie sie dodawac wiecej pol
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"    
 
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
 
-    profile_picture = models.ImageField(blank=True, null=True)
-    special_files = models.FileField(blank=True, null=True)
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email

db06c4cbe096865b6c7750492600a9311a45b0b6
kubusbubu <kubakarczewski25@gmail.com>
2023-11-12 23:04:29 +0100

    signals init

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 
@@ -11,6 +12,7 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
@@ -27,3 +29,10 @@ class Agent(models.Model):
 
     def __str__(self):
         return self.user.email
+
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(post_user_created_signal, sender=User)

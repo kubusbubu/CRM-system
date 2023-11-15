ea4fbe79fda8e7f9ab0cbd8662514cbad10d177a
kubusbubu <kubakarczewski25@gmail.com>
2023-11-15 20:00:40 +0100

    Category list view

--- leads/models.py
+++ leads/models.py
@@ -21,6 +21,7 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.SET_NULL)
     def __str__(self):
         return f"{self.first_name} {self.last_name}"    
 
@@ -31,6 +32,13 @@ class Agent(models.Model):
 
     def __str__(self):
         return self.user.email
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30) # New, Contacted, Converted, Unconverted
+    organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
 
 def post_user_created_signal(sender, instance, created, **kwargs):

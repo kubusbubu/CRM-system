6d30aeff3f9a688fc4ac2f387325ae0e9c3f8f97
kubusbubu <kubakarczewski25@gmail.com>
2024-01-18 20:33:35 +0100

    crispy forms

--- crm/settings.py
+++ crm/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
+    'crispy_forms',
+    'crispy_tailwind',
+
+    # my apps
     'leads',
     'agents'
 ]
@@ -130,3 +136,6 @@ AUTH_USER_MODEL = 'leads.User'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 LOGIN_REDIRECT_URL = "/leads"
 LOGIN_URL = "/login"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = 'tailwind'
